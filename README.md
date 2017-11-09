a8f818db84505abd112ccfe28329274d18d4c356
cuixianchao <15077550@qq.com>
2017-11-09 18:44:22 +0800

    osrange v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# osrange
-open source javascript range plugin
+![osrange](logo.png "osrange")
+
+# 原生js实现的宽度自适应range插件，也可用作进度条
+
+**实现原因**:
+
+  &emsp;1、视频全屏播放时需要一个宽度自适应的进度条
+
+  &emsp;2、浏览器自带的 input range 自定义样式兼容性差
+
+**优点**:
+
+  &emsp;1、样式控制灵活
+
+  &emsp;2、使用简单
+
+  &emsp;3、宽度自适应
+
+  &emsp;4、支持多个实例
+
+**缺点**:
+
+  &emsp;1、需要同时写css
+
+  &emsp;2、样式比较简陋
+
+## 获取和引用 osrange
+
+**简单粗暴方式（必须首先提供的方式）**
+
+  [`下载最新版本`](https://github.com/oscxc/osrange/releases) && 使用标签引用
+
+```
+<script src="osrange.js"></script>
+```
+
+**npm + CommonJS 方式**
+
+```
+npm install osrange
+```
+
+```
+var osrange = require('osrange');
+```
+
+## Usage examples
+
+1、基本使用（宽度固定）：[`examples/basic.html`](examples/basic.html)
+
+2、宽度自适应（百分比）：[`examples/adaptive.html`](examples/adaptive.html)
+
+3、setValue 和 getValue ：[`examples/value.html`](examples/value.html)
+
+4、用作进度条：[`examples/progress.html`](examples/progress.html)
+
+5、多个实例：[`examples/instances.html`](examples/progress.html)
+
+
+## License
+
+[MIT](LICENSE)
+
---
+++ osrange.js
@@ -0,0 +1,100 @@
+// osrange.js
+// author：七八个星天怪
+// LICENSE：MIT
+
+'use strict';
+window.osrange = function (mycfg) {
+    function extend(t,o) {
+        for(var n in o){
+            t[n]=o[n] ;
+        }
+    }
+    function createDiv() {
+        return document.createElement('div');
+    }
+    function css(el,obj) {
+        for(var k in obj){
+            el.style[k] = obj[k];
+        }
+    }
+
+    var cfg = {
+        id:null,
+        range:[0,1],
+        value:0,
+        control:true,
+        callback:null
+    };
+    extend(cfg,mycfg);
+
+    var bg = document.getElementById(cfg.id);
+    bg.style.cursor = 'default';
+    var min = cfg.range[0];
+    var max = cfg.range[1];
+    var value = cfg.value;
+    var cb = cfg.callback;
+
+    var dv = max - min;
+    var p = (value - min) / dv;
+
+    function getVal() {
+        return (value = p * dv + min);
+    }
+
+    var progress = createDiv();
+
+    function setW() {
+        progress.style.width = p * 100 + '%';
+    }
+    css(progress,{
+        position:'relative',
+        height: 'inherit',
+    });
+    setW();
+    cb && cb(getVal());
+
+    if(cfg.control){
+        bg.style.cursor = 'pointer';
+
+        var ball = createDiv();
+        var h = bg.clientHeight;
+        var ballWH = h * 2;
+
+        css(ball,{
+            position:'absolute',
+            width: ballWH + 'px',
+            height: ballWH + 'px',
+            right: -ballWH / 2 + 'px',
+            top: -(ballWH - h) / 2 + 'px'
+        });
+
+        progress.appendChild(ball);
+
+        ball.onmousedown = bg.onmousedown = function (e) {
+            e.stopPropagation();
+            var rect = bg.getBoundingClientRect();
+
+            document.onmousemove = (function handle(e) {
+                p = (e.clientX - rect.left) / rect.width;
+                p = p < 0 ? 0 : p > 1 ? 1 : p;
+                setW();
+                cb && cb(getVal());
+                return handle;
+            })(e);
+            document.onmouseup = function () {
+                document.onmousemove = null;
+            };
+        };
+    }
+
+    bg.appendChild(progress);
+
+    this.setValue = function (val) {
+        value = val < min ? min : val > max ? max : val;
+        p = (value - min) / dv;
+        setW();
+    };
+    this.getValue = function () {
+        return value;
+    };
+};
